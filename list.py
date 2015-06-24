14cdaf1f0636608e85ebe214acd456c8e495932e
David Radcliffe <dradcliffe@gmail.com>
2015-06-24 17:17:55 -0500

    increase limit to 999

--- list.py
+++ list.py
@@ -9,7 +9,7 @@ class ListHandler(webapp2.RequestHandler):
     def get(self):
         query = Location.query(ancestor=ancestor())
         locations = []
-        for loc in query.fetch(50):
+        for loc in query.fetch(999):
             dic = {'id': loc.key.id(),
                    'latitude': loc.coordinates.lat,
                    'longitude': loc.coordinates.lon,
