ac9724a24d62c2b74236e20e860beff16507584f
David Radcliffe <dradcliffe@gmail.com>
2015-06-26 18:43:09 -0500

    ajax debugging

--- scripts/main.js
+++ scripts/main.js
@@ -90,8 +90,25 @@ function initialize() {
 
 function getMarkers() {
     $.ajax({
-        type: 'GET', url: 'list', success: setMarkers
-    })
+        type: 'GET', url: 'list', success: setMarkers,
+        error: function(jqXHR, exception) {
+            if (jqXHR.status === 0) {
+                alert('Not connect.n Verify Network.');
+            } else if (jqXHR.status == 404) {
+                alert('Requested page not found. [404]');
+            } else if (jqXHR.status == 500) {
+                alert('Internal Server Error [500].');
+            } else if (exception === 'parsererror') {
+                alert('Requested JSON parse failed.');
+            } else if (exception === 'timeout') {
+                alert('Time out error.');
+            } else if (exception === 'abort') {
+                alert('Ajax request aborted.');
+            } else {
+                alert('Uncaught Error.n' + jqXHR.responseText);
+            }
+        }
+    });
 }
 
 var icons = ['icons/blue-dot.png',
